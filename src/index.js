71ecaf25ea6c752734e4110d5a2fecb7334064b7
eapepe0 <eapepe0@gmail.com>
2023-05-05 16:18:08 -0300

    Leccion 200 Continuacion de la aplicacion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 import './styles.css';
 
@@ -12,4 +12,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
