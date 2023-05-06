9a85c9a20837984096bce1d14fbbe17b9b9cda43
eapepe0 <eapepe0@gmail.com>
2023-05-05 22:24:11 -0300

    Leccion 203 y 204 , Workbox Cache Strategies , Verificacion de Token offline

--- src/sw-template.js
+++ src/sw-template.js
@@ -4,3 +4,30 @@ importScripts(
   );
 
 workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst , NetworkFirst } = workbox.strategies;
+
+
+
+
+registerRoute(
+    new RegExp("https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"),
+    new CacheFirst()
+)
+
+registerRoute(
+    new RegExp("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"),
+    new CacheFirst()
+)
+
+registerRoute(
+    new RegExp("http://localhost:4000/api/auth/renew"),
+    new NetworkFirst()
+)
+
+registerRoute(
+    new RegExp("http://localhost:4000/api/events"),
+    new NetworkFirst()
+)
+
